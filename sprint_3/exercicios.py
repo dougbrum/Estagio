2a00da6929fbb80d647655f8d9530dc8e7ce5f3e
dougbrum <dougbrum@hotmail.com>
2023-02-27 23:36:22 -0300

    Completando os 25 exercicios

--- sprint_3/exercicios.py
+++ sprint_3/exercicios.py
@@ -326,3 +326,191 @@ print(f'Media: {media}, Mediana: {mediana}, Mínimo: {valor_minimo}, Máximo: {v
 
 a = [1, 0, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 print(a[::-1])
+
+
+#Parte 3
+#E21
+#Implemente duas classes Pato e Pardal que herdem de uma classe Passaro a habilidade de voar e emitir som, porém, tanto Pato quanto Pardal devem emitir sons diferentes (de maneira escrita) no console.
+#Imprima no console exatamente assim:
+#Pato
+#Voando...
+#Pato emitindo som...
+#Quack Quack
+#Pardal
+#Voando...
+#Pardal emitindo som...
+#Piu Piu
+
+class Passaro:
+    def __init__(self, nome):
+        self.nome = nome
+        print(self.nome)
+        
+    def voar(self):
+        print('Voando...')
+        
+    def emitir_som(self):
+        pass
+        
+        
+class Pato(Passaro):
+    
+    def __init__(self, nome):
+        super().__init__(nome)
+        
+    def emitir_som(self):
+        print('Pato emitindo som...')
+        print('Quack Quack')
+        
+        
+class Pardal(Passaro):
+    
+    def __init__(self, nome):
+        super().__init__(nome)
+        
+    def emitir_som(self):
+        print('Pardal emitindo som...')
+        print('Piu Piu')
+        
+p = Pato('Pato')
+p.voar()
+p.emitir_som()
+pard = Pardal('Pardal')
+pard.voar()
+pard.emitir_som()
+
+#E22
+#Crie uma classe chamada "Pessoa" com um atributo privado "nome" (representado como "__nome") e um atributo público "id". Adicione duas funções à classe, uma para definir o valor de "nome" e outra para obter o valor de "nome". Observe que o atributo "nome" deve ser privado e acessado somente através dessas funções.
+#Para testar seu código use:
+#pessoa = Pessoa(0) 
+#pessoa.nome = 'Fulano De Tal'
+#print(pessoa.nome)
+
+class Pessoa:
+    def __init__(self, id):
+        self.id = id
+        
+    def get_nome(self):
+        return self.__nome
+        
+    def nome(self, nome):
+        self.__nome = nome
+        
+        
+pessoa = Pessoa(0)        
+pessoa.nome('Fulano de Tal')        
+print(pessoa.get_nome())
+
+
+""" O código acima é o correto segundo o verificador da trilha, mas confesso que achei a forma que fiz abaixo mais fiel ao que foi solicitado pelo enunciado. Pois a questão diz que a classe Pessoa tem dois atributos, nome e id. Entao imaginei que tais atributos precisariam fazer parte do metodo construtor. Porem quando é fornecida a sugestão de teste o objeto criado recebe apenas um parâmetro, o id, por isso imaginei que o código acima funcionaria melhor com o verificador de soluções. """
+
+
+class Pessoa:
+    def __init__(self, nome, id):
+        self.__nome = nome
+        self.id = id
+        
+    def get_nome(self):
+        return self.__nome
+        
+    def nome(self, nome):
+        self.__nome = nome
+        
+        
+pessoa = Pessoa('Douglas',0)        
+pessoa.nome('Fulano de Tal')        
+print(pessoa.get_nome())
+
+
+#E23
+#Crie uma classe  Calculo  que contenha um método que aceita dois parâmetros, X e Y, e retorne a soma dos dois. Nessa mesma classe, implemente um método de subtração, que aceita dois parâmetros, X e Y, e retorne a subtração dos dois (resultados negativos são permitidos).
+#Utilize os valores abaixo para testar seu exercício:
+#x = 4 
+#y = 5
+#imprima:
+#Somando: 4+5 = 9
+#Subtraindo: 4-5 = -1
+
+
+class Calculo:
+    def __init__(self):
+        pass
+        
+    def soma(self, x, y):
+        return x + y
+        
+    def subtracao(self, x, y):
+        return x - y
+        
+        
+calc1 = Calculo()
+print(calc1.soma(4, 5))
+print(calc1.subtracao(4, 5))
+
+
+#E24
+#Crie uma classe Ordenadora que contenha um atributo listaBaguncada e que contenha os métodos ordenacaoCrescente e ordenacaoDecrescente.
+#Instancie um objeto chamado crescente dessa classe Ordenadora que tenha como listaBaguncada a lista [3,4,2,1,5] e instancie um outro objeto, decrescente dessa mesma classe com uma outra listaBaguncada sendo [9,7,6,8].
+#Para o primeiro objeto citado, use o método ordenacaoCrescente e para o segundo objeto, use o método
+#ordenacaoDecrescente.
+#Imprima o resultado da ordenação crescente e da ordenação decresce
+#[1, 2, 3, 4, 5] 
+#[9, 8, 7, 6]
+
+
+class Ordenadora:
+    
+    def __init__(self, listaBaguncada):
+        self.listaBaguncada = listaBaguncada
+        
+    def ordenacaoCrescente(self):
+        self.listaBaguncada.sort(reverse = False)
+        return self.listaBaguncada
+        
+    
+    def ordenacaoDecrescente(self):
+        self.listaBaguncada.sort(reverse = True)
+        return self.listaBaguncada
+        
+lc = Ordenadora([3,4,2,1,5])
+ld = Ordenadora([9,7,6,8])
+
+print(lc.ordenacaoCrescente())
+print(ld.ordenacaoDecrescente())
+
+
+
+#E25
+#Crie uma classe Avião que possua os atributos modelo, velocidade_maxima, cor e capacidade.
+#Defina o atributo cor de sua classe , de maneira que todas as instâncias de sua classe avião sejam da cor “azul”.
+#Após isso, a partir de entradas abaixo, instancie e armazene em uma lista 3 objetos da classe Avião.
+#Ao final, itere pela lista imprimindo cada um dos objetos no seguinte formato:
+#“O avião de modelo “x” possui uma velocidade máxima de “y”, capacidade para “z” passageiros e é da cor “w”.
+#Sendo x, y, z e w cada um dos atributos da classe “Avião”.
+#Valores de entrada:
+#modelo BOIENG456: velocidade máxima 1500 km/h: capacidade para 400 passageiros: Cor Azul
+#modelo Embraer Praetor 600: velocidade máxima 863km/h: capacidade para 14 passageiros: Cor Azul
+#modelo Antonov An-2: velocidade máxima de 258 Km/h: capacidade para 12 passageiros: Cor Azul
+
+
+class Aviao:
+    
+    def __init__(self, modelo, velocidade_maxima, capacidade):
+        self.modelo = modelo
+        self.velocidade_maxima = velocidade_maxima
+        self.cor = 'azul'
+        self.capacidade = capacidade
+lista = []    
+    
+obj1 = Aviao('BOIENG456', '1500 km/h', 400)
+obj2 = Aviao('Embraer Praetor 600', '863km/h', 14)
+obj3 = Aviao('Antonov An-2', '258 Km/h', 12)
+    
+lista.append(obj1)
+lista.append(obj2)
+lista.append(obj3)
+    
+    
+for i in range(len(lista)):
+    print (f'O avião de modelo {lista[i].modelo} possui uma velocidade máxima de {lista[i].velocidade_maxima}, capacidade para {lista[i].capacidade} passageiros e é da cor {lista[i].cor}')
+    
